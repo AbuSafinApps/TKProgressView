b0241a23be3f28cea74fa959f1678807ab99366b
Toseefhusen <toseefhusen@webteamcorp.com>
2018-02-14 10:06:09 +0530

    Added License In all file Example added

--- TKProgressView.podspec
+++ TKProgressView.podspec
@@ -1,20 +1,15 @@
 
 Pod::Spec.new do |s|
 s.name             = 'TKProgressView'
-s.version          = '0.0.1'
-s.summary          = 'Animated view'
-
-s.description      = <<-DESC
-This Progress view changes its color gradually makes your app look fantastic!
-DESC
-
+s.version          = '0.0.2'
+s.summary          = '🌀Amazing animated activity indicator written in swift.'
+s.description      = 'This Progress view changes its color dots and postion gradually makes your app look great!'
 s.homepage         = 'https://github.com/toseefkhilji/TKProgressView'
-s.license          = { :type => 'MIT', :file => 'LICENSE' }
+s.license          = { :type => 'MIT', :file => 'LICENSE.md' }
 s.author           = { 'Toseef Khilji' => 'hi@ASApps.com' }
 s.source           = { :git => 'https://github.com/toseefkhilji/TKProgressView.git', :tag => s.version.to_s }
-
+s.platform     = :ios, '9.0'
 s.ios.deployment_target = '9.0'
-s.source_files = 'TKProgressView/*.{swift,plist}'
-
+s.source_files = 'TKProgressView/Classes/*.swift'
+s.requires_arc = true
 end
-
--- TKProgressView/ViewController.swift
+++ TKProgressView/ViewController.swift
@@ -2,20 +2,52 @@
 //  ViewController.swift
 //  TKProgressView
 //
-//  Created by Toseefhusen on 2/13/18.
-//  Copyright © 2018 Toseefhusen. All rights reserved.
+//    MIT License
 //
+//    Copyright (c) 2018 Toseef Khilji
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tkView: TKProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let tkView: TKProgressView = TKProgressView.init(frame: CGRect.init(x: 50, y: 50, width: 100, height: 100 ))
+        
+        tkView = TKProgressView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
         self.view.addSubview(tkView)
         tkView.startAnimating()
+    }
+    
+    @IBAction func btnAction(_ sender: UIButton) {
+        switch sender.title(for: .normal)! {
+        case "Start":
+            tkView?.startAnimating()
+            sender.setTitle("Stop", for: .normal)
+        case "Stop":
+            tkView?.stopAnimating()
+            sender.setTitle("Start", for: .normal)
+        default:
+            break;
+        }
     }
 }
 
